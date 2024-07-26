05ca152926e67bac56e089e81b3887f5efdb93c6
Aaron Wright <aaronmw@gmail.com>
2024-07-26 16:05:44 -0400

    Tweak

--- src/generate-contentful-types.sh
+++ src/generate-contentful-types.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-source ../../../.env.local
+source ../../.env.local
 
 cf-content-types-generator --v10 --spaceId $CONTENTFUL_SPACE_ID --token $CONTENTFUL_MANAGEMENT_ACCESS_TOKEN --out ./src/lib/contentfulTypes
